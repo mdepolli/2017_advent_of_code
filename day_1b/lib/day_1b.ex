fc64167d2142ad91d880c2961b375e063dadd118
Marcelo De Polli <marcelo.polli@gmail.com>
2017-12-01 23:58:33 -0200

    Refactor Day 1b with Stream

--- day_1b/lib/day_1b.ex
+++ day_1b/lib/day_1b.ex
@@ -15,20 +15,17 @@ defmodule Day1b do
   def solve(data) do
     data = data |> treat_data
 
-    0..(length(data) - 1)
-    |> Enum.map(&to_tuples(data, &1))
-    |> Enum.filter(fn(ele) -> match?({a, a}, ele) end)
-    |> Enum.reduce(0, fn({ele, ele}, acc) -> String.to_integer(ele) + acc end)
+    data
+    |> Stream.with_index
+    |> Stream.filter(fn({ele, index}) -> ele == Enum.at(data, matching_index(length(data), index)) end)
+    |> Stream.map(fn({ele, _}) -> String.to_integer(ele) end)
+    |> Enum.sum
   end
 
   defp treat_data(str) do
     str
     |> String.trim
     |> String.split("", trim: true)
-  end
-
-  defp to_tuples(data, index) do
-    {Enum.at(data, index), Enum.at(data, matching_index(length(data), index))}
   end
 
   defp matching_index(size, index) when index < round(size / 2), do: index + round(size / 2)
